2f3319742999e9543552eb2430ea9e34b6c6efd5
enzokrebs8 <enzokrebs8@gmail.com>
2024-02-28 23:35:55 -0300

    Amém!

--- AULA 02_EXERCIO_LAMPADA/js/script.js
+++ AULA 02_EXERCIO_LAMPADA/js/script.js
@@ -1,40 +1,64 @@
 const ligar = document.getElementById('ligar')
 const desligar = document.getElementById('desligar')
 const lampada = document.getElementById('lamp')
+const string = document.getElementById('string')
+const botaoDuplo = document.getElementById('botaoDuplo')
 const restaurar = document.getElementById('restaurar')
 
-
-
-function lampLigada() {
-    if (!estaQuebrada()) {
-        lampada.src = 'img/ligada.jpg'
-    }
-}
-
-function lampDesligada() {
-    if (!estaQuebrada()) {
-        lampada.src = 'img/desligada.jpg'
-    }
-}
-
-function lampQuebrada() {
-    lampada.src = 'img/quebrada.jpg'
-}
-
-function estaQuebrada() {
+function estaQuebrada(){
     return lampada.src.indexOf('quebrada') > -1
-    //Isso retorna True ou False
 }
 
-function lampRestaurar() {
-    if (estaQuebrada()) {
-        lampada.src = 'img/desligada.jpg'
+function lampLigada(){
+    if(!estaQuebrada()){
+        lampada.src = "img/ligada.jpg"
+        string.innerHTML = "A LÂMPADA ESTÁ LIGADA"
+    }
+}
+
+function lampDesligada(){
+    if(!estaQuebrada()){
+        lampada.src = "img/desligada.jpg"
+        string.innerHTML = "A LÂMPADA ESTÁ DESLIGADA"
+    }
+}
+
+function lampQuebrada(){
+    lampada.src = "img/quebrada.jpg"
+    string.innerHTML = "A LÂMPADA ESTÁ QUEBRADA"
+}
+
+function estInicial(){
+    return lampada.src.indexOf('desligada') > -1
+}
+
+function Duplo(){
+    if(!estaQuebrada()){
+        if(estInicial()){
+            lampada.src = "img/ligada.jpg"
+            string.innerHTML = "A LÂMPADA ESTÁ LIGADA"
+        }
+        else{
+            lampada.src = "img/desligada.jpg"
+            string.innerHTML = "A LÂMPADA ESTÁ DESLIGADA"
+        }
+       
+    }
+}
+
+function restaura(){
+    if(estaQuebrada()){
+        lampada.src = "img/desligada.jpg"
+        string.innerHTML = "A LÂMPADA ESTÁ DESLIGADA"
     }
 }
 
 ligar.addEventListener('click', lampLigada)
 desligar.addEventListener('click', lampDesligada)
+restaurar.addEventListener('click', restaura)
+botaoDuplo.addEventListener('click', Duplo)
+
 lampada.addEventListener('mouseover', lampLigada)
 lampada.addEventListener('mouseleave', lampDesligada)
+
 lampada.addEventListener('dblclick', lampQuebrada)
-restaurar.addEventListene('click', lampRestaurar)
