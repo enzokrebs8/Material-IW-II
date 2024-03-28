ad5709ec74a404608029c0271eaa92bcdf928fc9
enzokrebs8 <enzokrebs8@gmail.com>
2024-03-28 11:34:03 -0300

    Homework

--- AULA_06/IW-ManipulandoCSS/js/script.js
+++ AULA_06/IW-ManipulandoCSS/js/script.js
@@ -15,38 +15,82 @@ titulo.style.borderBottom = "2px solid red";
 titulo.style.padding = "0.625rem";
 titulo.style.borderRadius = "5px";
 
-//let box = document.querySelectorAll('.box')
+let box = document.querySelectorAll('.box')
 box[0].setAttribute('class', 'escura')
-//box[0].removeAttribute('class')
+box[0].removeAttribute('class')
 
 /////// MODOS DE COR ///////
-//let tela = document.querySelector('main')
+let tela = document.querySelector('main')
 
-//let btnDark = document.querySelector('#btdark')
+let btnDark = document.querySelector('#btdark')
 
-//let btnLight = document.querySelector('#btlight')
+let btnLight = document.querySelector('#btlight')
 
-//btnDark.addEventListener('click', modoDark)
-//btnLight.addEventListener('click', modoLight)
+let btnRosa = document.querySelector('#btrosa')
+
+let btnAzul = document.querySelector('#btazul')
+
+let btnAmarelo = document.querySelector('#btamarelo')
+
+btnDark.addEventListener('click', modoDark)
+btnLight.addEventListener('click', modoLight)
+btnRosa.addEventListener('click', modoRosa)
+btnAzul.addEventListener('click', modoAzul)
+btnAmarelo.addEventListener('click', modoAmarelo)
 
 function modoDark() {
-    //event.preventDefault();
+    event.preventDefault();
     console.log('modo dark')
     tela.classList.add("dark");
     tela.classList.remove("light");
+    tela.classList.remove("rosa");
+    tela.classList.remove("azul");
+    tela.classList.remove("amarelo");
 }
 
 function modoLight() {
-    //event.preventDefault();
+    event.preventDefault();
     console.log('modo light')
     tela.classList.remove("dark");
+    tela.classList.remove("rosa");
+    tela.classList.remove("azul");
+    tela.classList.remove("amarelo");
     tela.classList.add("light");
 }
 
-/*
+function modoRosa() {
+    event.preventDefault();
+    console.log('modo rosa')
+    tela.classList.remove("dark");
+    tela.classList.remove("light");
+    tela.classList.remove("azul");
+    tela.classList.remove("amarelo");
+    tela.classList.add("rosa");
+}
+
+function modoAzul() {
+    event.preventDefault();
+    console.log('modo light')
+    tela.classList.remove("dark");
+    tela.classList.remove("rosa");
+    tela.classList.remove("light");
+    tela.classList.remove("amarelo");
+    tela.classList.add("azul");
+}
+
+function modoAmarelo() {
+    event.preventDefault();
+    console.log('modo light')
+    tela.classList.remove("dark");
+    tela.classList.remove("rosa");
+    tela.classList.remove("azul");
+    tela.classList.remove("light");
+    tela.classList.add("amarelo");
+}
+
 titulo.addEventListener('click', mudarId)
 function mudarId() {
     titulo.id = 'titulo'
     console.log(titulo.id)
 }
-*/
+
